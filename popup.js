fb376d0a0c3b58268cfde14bb509ed0243aed500
Aarif <51032249+aarif-razak@users.noreply.github.com>
2020-06-25 16:32:26 -0400

    Updated the reset button to refresh page

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var resetGameButton = document.getElementById("resetGame");
+
+checkPageButton.addEventListener('click', function() {
+    location.reload();
+},false);
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
